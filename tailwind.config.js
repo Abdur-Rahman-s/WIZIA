c5c76b0992c1f0b181f2bc76b335e82d66f22809
Abdur-Rahman-s <arsw5649@gmail.com>
2024-11-27 18:37:46 +0600

    Tailwind config js includes frequently used animation font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 const { nextui } = require("@nextui-org/react");
 
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-
     extend: {
       fontFamily: {
-        "Work_Sans": ['"Work Sans", sans-serif'],
-        "Montserrat": ['"Montserrat", sans-serif'],
+        Work_Sans: ['"Work Sans"', "sans-serif"],
+        Montserrat: ['"Montserrat"', "sans-serif"],
       },
       animation: {
-        'slide-down': 'slideDown 0.3s ease-in-out', // Add the custom animation class
+        'slide-down': 'slideDown 0.3s ease-in-out',
+        'gradient-flow': 'gradientFlow 8s ease infinite',
+        'fade-in': 'fadeIn 2s ease forwards',
+        'slide-up': 'slideUp 1.5s ease forwards',
       },
       keyframes: {
         slideDown: {
           '0%': { transform: 'translateY(-10px)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideUp: {
+          '0%': { transform: 'translateY(50px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        gradientFlow: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
       },
-      backgroundImage : {
-        Hero_Image : "url('./public/Hero.png')",
-      }
+      backgroundImage: {
+        Hero_Image: "url('./public/Hero.png')",
+      },
     },
-    
   },
   darkMode: "class",
   plugins: [nextui()],
